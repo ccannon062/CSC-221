62d226c1d37c3d00dab8b7ac153c030c37443a81
Caleb C <118626040+ccannon062@users.noreply.github.com>
2024-03-18 11:12:16 -0500

    Add files via upload

---
+++ Circumference.py
@@ -0,0 +1,12 @@
+#Caleb Cannon
+#SimpleCirc.py
+#A program that finds the square inches and perimiter of a rectangle
+
+length = int(input("Please enter a rectangle length: "))
+width = int(input("Please enter a width for a rectangle: "))
+square_inch = length*width
+perimeter = 2*length+2*width
+print("Length:", length)
+print("Width:", width)
+print("Square Inch:", square_inch)
+print("Perimeter:", perimeter)
