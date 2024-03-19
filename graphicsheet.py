51a8802fe33926eae11330651feb40a01fa48b8b
Caleb C <118626040+ccannon062@users.noreply.github.com>
2024-03-19 14:31:52 -0500

    Add files via upload

---
+++ graphicsheet.py
@@ -0,0 +1,35 @@
+# graphicsSheet.py
+# Caleb Cannon
+# A file that tests the graphics.py library\
+
+from graphics import *
+
+def main():
+
+    
+    win = GraphWin("Sheet Proof")
+    
+    """
+    c = Circle(Point(30,40),25)
+    c.setFill('blue')
+    c.setOutline('red')
+    c.draw(win)
+    
+    r = Rectangle(Point(20,20), Point(40,40))
+    r.setFill(color_rgb(0,255,150))
+    r.setWidth(3)
+    r.draw(win)
+    
+    l = Line(Point(100,100),Point(100,200))
+    l.setOutline('red4')
+    l.setArrow('first')
+    l.draw(win)
+
+    Oval(Point(50,50),Point(60,100)).draw(win)
+
+    shape = Polygon(Point(5,5),Point(50,50),Point(5,50),Point(50,5))
+    shape.setFill('Orange')
+    shape.draw(win)
+    """
+    
+main()
